dade73ab5aac6ad0c14b2126ef43e46df8ad18ed
Vuk Mandić <135885855+vmandic0@users.noreply.github.com>
2026-03-31 15:02:52 +0200

    Creating contract skeleton
    Initial implementation of GymMembershipVault contract for managing gym memberships.

--- GymMembershipVault.sol
+++ GymMembershipVault.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+/**
+ * @title GymMembershipVault
+ * @author Vuk
+ * @notice Pametni ugovor za upravljanje članarinama u teretani na Sepolia testnoj mreži.
+ */
+
+contract GymMembershipVault {
+    address public owner;
+    uint256 public membershipPrice = 0.01 ether;
+    mapping(address => bool) public isMember;
+
+    constructor() {
+        owner = msg.sender;
+    }
+}
